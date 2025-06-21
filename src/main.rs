9865e5e95c81b8c5dbfd302ac73fe2720c1e4580
Takuya Saito <takus4649@gmail.com>
2025-06-21 18:32:04 +0900

    v2: 右下から乗せられる箱をビームサーチ。評価関数ははは乗せる箱の座標2(提出: 1,678,428, 予測: 1,676,674)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use proconio::input;
 use std::time::{Duration, Instant};
+use std::collections::BinaryHeap;
+use std::cmp::Reverse;
 
 struct Solver {
     n: usize,
@@ -123,7 +125,81 @@ impl Solver {
                 self.r -= carry.len();
             }
         }
+        self.ans = ans.clone();
+        let mut opt_score = self.score();
+        let mut opt_ans = ans;
+
+        // solverをリセット
+        self.board = vec![vec![true; self.n]; self.n];
+        self.board[0][0] = false;
+        self.t = 0;
+        self.r = self.n*self.n - 1;
+        self.ans = Vec::new();
+
+        let mut ans: Vec<char> = Vec::new();
+        for s in (1..(self.n+self.n-1)).rev() {
+            for i in 0..=(s.min(self.n-1)) {
+                let j = s-i;
+                if j >= self.n { continue; }
+                if !self.carriable(i, j, &Vec::new()) { continue; }
+
+                // ビームサーチ
+                let beam_width = 10;
+                let mut beams: BinaryHeap<(Reverse<usize>, Vec<(usize, usize)>, Vec<(usize, usize, usize)>)> = BinaryHeap::new();
+                beams.push((Reverse(0), vec![(i, j)], vec![(self.w[i][j], self.d[i][j], 0)]));
+                let mut opt_beams: BinaryHeap<(usize, Vec<(usize, usize)>)> = BinaryHeap::new();
+                while let Some((Reverse(eval), order, carry)) = beams.pop() {
+                    let mut next_beams: BinaryHeap<(Reverse<usize>, Vec<(usize, usize)>, Vec<(usize, usize, usize)>)> = BinaryHeap::new();
+                    let &(i, j) = order.last().unwrap();
+                    // 次に乗せる箱の候補を確認
+                    for s in (0..(i+j)).rev() {
+                        for i2 in 0..=(s.min(i)) {
+                            let j2 = s-i2;
+                            if j2 > j { continue; }
+                            if !self.carriable(i2, j2, &carry) { continue; }
+                            let mut next_order = order.clone();
+                            next_order.push((i2, j2));
+                            let mut next_carry = carry.clone();
+                            // 消費耐久力の更新
+                            let lost = self.w[i2][j2]*(i2+j2);
+                            for i in 0..carry.len() {
+                                next_carry[i].2 += lost;
+                            }
+                            next_carry.push((self.w[i2][j2], self.d[i2][j2], 0));
+                            next_beams.push((Reverse(eval+(i2+j2)*2), next_order, next_carry));
+                            if next_beams.len() > beam_width { next_beams.pop(); }
+                        }
+                    }
+                    // eprintln!("next_beams: {:?}", next_beams);
+                    if beams.is_empty() {
+                        opt_beams.push((eval, order.clone()));
+                        beams = next_beams;
+                    }
+                }
+                
+                // 評価値が一番高い処理を実施
+                // eprintln!("opt_beams: {:?}", opt_beams);
+                let (_, order) = opt_beams.pop().unwrap();
+
+                // 運び出し開始
+                let mut carry: Vec<(usize, usize, usize)> = Vec::new();  // (重さ、耐久力、消消耐久力)
+                let mut now_i = 0;
+                let mut now_j = 0;
+                // eprintln!("order: {}, {:?}", order.len(), order);
+                for &(i, j) in order.iter() {
+                    (now_i, now_j) = self.r#move(now_i, now_j, i, j, &mut ans);
+                    self.carry(i, j, &mut carry, &mut ans);
+                }
+                (now_i, now_j) = self.r#move(now_i, now_j, 0, 0, &mut ans);
+                self.r -= carry.len();
+            }
+        }
         self.ans = ans;
+
+        let score = self.score();
+        // if opt_score > score {
+        //    self.ans = opt_ans;
+        //}
 
         eprintln!("solve end: {} ms", self.duration());
     }
