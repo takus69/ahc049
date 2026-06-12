c9aed9bc1fb8be1fcfcc4da9c5de5cad5a15a1d2
Takuya Saito <takus4649@gmail.com>
2026-06-13 04:43:56 +0900

    commit

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ struct Solver {
     r: usize,
     t: usize,
     ans: Vec<char>,
+    score: usize,
     timer: Instant,
 }
 
@@ -28,9 +29,10 @@ impl Solver {
         let t = 0;
         let r = n*n - 1;
         let ans: Vec<char> = Vec::new();
+        let score = 0;
         let timer = Instant::now();
 
-        Self { n, w, d, board, t, r, ans, timer }
+        Self { n, w, d, board, t, r, ans, score, timer }
     }
 
     fn duration(&self) -> usize {
@@ -126,6 +128,7 @@ impl Solver {
             }
         }
         self.ans = ans.clone();
+        self.score = self.score();
         let mut opt_score = self.score();
         let mut opt_ans = ans;
 
@@ -137,6 +140,7 @@ impl Solver {
         self.ans = Vec::new();
 
         let mut ans: Vec<char> = Vec::new();
+        let mut cnt: usize = 0;
         for s in (1..(self.n+self.n-1)).rev() {
             for i in 0..=(s.min(self.n-1)) {
                 let j = s-i;
@@ -144,7 +148,7 @@ impl Solver {
                 if !self.carriable(i, j, &Vec::new()) { continue; }
 
                 // ビームサーチ
-                let beam_width = 10;
+                let beam_width = 100;
                 let mut beams: BinaryHeap<(Reverse<usize>, Vec<(usize, usize)>, Vec<(usize, usize, usize)>)> = BinaryHeap::new();
                 beams.push((Reverse(0), vec![(i, j)], vec![(self.w[i][j], self.d[i][j], 0)]));
                 let mut opt_beams: BinaryHeap<(usize, Vec<(usize, usize)>)> = BinaryHeap::new();
@@ -192,14 +196,104 @@ impl Solver {
                 }
                 (now_i, now_j) = self.r#move(now_i, now_j, 0, 0, &mut ans);
                 self.r -= carry.len();
+                cnt += 1;
             }
         }
-        self.ans = ans;
+        self.ans = ans.clone();
 
         let score = self.score();
-        // if opt_score > score {
-        //    self.ans = opt_ans;
-        //}
+        eprintln!("score: {}, cnt: {}", score, cnt);
+        if opt_score > score {
+            self.ans = opt_ans.clone();
+        } else {
+            opt_score = score;
+            opt_ans = ans;
+            self.score = score;
+        }
+
+        // solverをリセット
+        self.board = vec![vec![true; self.n]; self.n];
+        self.board[0][0] = false;
+        self.t = 0;
+        self.r = self.n*self.n - 1;
+        self.ans = Vec::new();
+
+        // 耐久力が高い箱から実施
+        let mut heap: BinaryHeap<(usize, usize, usize, usize)> = BinaryHeap::new();
+        for i in 0..self.n {
+            for j in 0..self.n {
+                if self.w[i][j] == 0 { continue; }
+                heap.push(((i+j)*(i+j)*self.d[i][j], self.w[i][j], i, j));
+            }
+        }
+
+        let mut ans: Vec<char> = Vec::new();
+        let mut cnt: usize = 0;
+        while let Some((_, _, i, j)) = heap.pop() {
+            if !self.carriable(i, j, &Vec::new()) { continue; }
+
+            // ビームサーチ
+            let beam_width = 100;
+            let mut beams: BinaryHeap<(Reverse<usize>, Vec<(usize, usize)>, Vec<(usize, usize, usize)>)> = BinaryHeap::new();
+            beams.push((Reverse(0), vec![(i, j)], vec![(self.w[i][j], self.d[i][j], 0)]));
+            let mut opt_beams: BinaryHeap<(usize, Vec<(usize, usize)>)> = BinaryHeap::new();
+            while let Some((Reverse(eval), order, carry)) = beams.pop() {
+                let mut next_beams: BinaryHeap<(Reverse<usize>, Vec<(usize, usize)>, Vec<(usize, usize, usize)>)> = BinaryHeap::new();
+                let &(i, j) = order.last().unwrap();
+                // 次に乗せる箱の候補を確認
+                for s in (0..(i+j)).rev() {
+                    for i2 in 0..=(s.min(i)) {
+                        let j2 = s-i2;
+                        if j2 > j { continue; }
+                        if !self.carriable(i2, j2, &carry) { continue; }
+                        let mut next_order = order.clone();
+                        next_order.push((i2, j2));
+                        let mut next_carry = carry.clone();
+                        // 消費耐久力の更新
+                        let lost = self.w[i2][j2]*(i2+j2);
+                        for i in 0..carry.len() {
+                            next_carry[i].2 += lost;
+                        }
+                        next_carry.push((self.w[i2][j2], self.d[i2][j2], 0));
+                        next_beams.push((Reverse(eval+(i2+j2)*2), next_order, next_carry));
+                        if next_beams.len() > beam_width { next_beams.pop(); }
+                    }
+                }
+                // eprintln!("next_beams: {:?}", next_beams);
+                if beams.is_empty() {
+                    opt_beams.push((eval, order.clone()));
+                    beams = next_beams;
+                }
+            }
+            
+            // 評価値が一番高い処理を実施
+            // eprintln!("opt_beams: {:?}", opt_beams);
+            let (_, order) = opt_beams.pop().unwrap();
+
+            // 運び出し開始
+            let mut carry: Vec<(usize, usize, usize)> = Vec::new();  // (重さ、耐久力、消消耐久力)
+            let mut now_i = 0;
+            let mut now_j = 0;
+            // eprintln!("order: {}, {:?}", order.len(), order);
+            for &(i, j) in order.iter() {
+                (now_i, now_j) = self.r#move(now_i, now_j, i, j, &mut ans);
+                self.carry(i, j, &mut carry, &mut ans);
+            }
+            (now_i, now_j) = self.r#move(now_i, now_j, 0, 0, &mut ans);
+            self.r -= carry.len();
+            cnt += 1;
+        }
+        self.ans = ans.clone();
+
+        let score = self.score();
+        eprintln!("score: {}, cnt: {}", score, cnt);
+        if opt_score > score {
+            self.ans = opt_ans.clone();
+        } else {
+            opt_score = score;
+            opt_ans = ans;
+            self.score = score;
+        }
 
         eprintln!("solve end: {} ms", self.duration());
     }
@@ -219,7 +313,7 @@ impl Solver {
     }
 
     fn result(&self) {
-        eprintln!("{{ \"n\": {}, \"score\": {} }}", self.n, self.score());
+        eprintln!("{{ \"n\": {}, \"score\": {} }}", self.n, self.score);
     }
 }
 
